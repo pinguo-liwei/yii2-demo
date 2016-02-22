281e9a404314237305c7d1f97a15dfc335f9c6df
pinguo-liwei <liwei@camera360.com>
2016-02-22 15:17:27 +0800

    add yii2-mongodb,yii2-redis,cookie key,gii

--- config/web.php
+++ config/web.php
@@ -4,12 +4,15 @@ $params = require(__DIR__ . '/params.php');
 
 $config = [
     'id' => 'basic',
+    'name' => 'Yii2 demo',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'language' => 'zh-CN',
+    'charset' => 'UTF-8',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'yii2_cookie_key',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -52,6 +55,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.*'], // 允许访问的IP段
     ];
 }
 
